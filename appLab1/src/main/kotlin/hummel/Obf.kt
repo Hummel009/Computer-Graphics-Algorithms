a9c9af91b58517cdc04fccae8ea0ed854e1045e9
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-01-31 14:26:17 +0300

    Update Obf.kt

--- appLab1/src/main/kotlin/hummel/Obf.kt
+++ appLab1/src/main/kotlin/hummel/Obf.kt
@@ -1,6 +1,6 @@
 package hummel
 
-infix fun MutableList<FloatArray>.of(i: Int): FloatArray = this[i - 1]
+infix fun List<FloatArray>.of(i: Int): FloatArray = this[i - 1]
 infix fun Float.transformX(pair: Pair<Float, Float>): Int = ((this - pair.first) * pair.second).toInt()
 infix fun Float.transformY(pair: Pair<Float, Float>): Int = ((this - pair.first) * pair.second).toInt()
 infix fun Int.invertAxisY(int: Int): Int = this - int
