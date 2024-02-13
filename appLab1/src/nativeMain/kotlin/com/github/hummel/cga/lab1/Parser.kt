89ce490a4ca12aad24f852e89b99839ff8d46af5
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-02-13 14:37:53 +0300

    1

--- appLab1/src/nativeMain/kotlin/com/github/hummel/cga/lab1/Parser.kt
+++ appLab1/src/nativeMain/kotlin/com/github/hummel/cga/lab1/Parser.kt
@@ -16,21 +16,14 @@ fun parse(fileName: String) {
 		val array = line.trim().split("\\s+".toRegex()).toTypedArray()
 
 		when (array[0]) {
-			"v" -> addVertex(array.drop(1).toTypedArray())
 			"f" -> addFace(array.drop(1).toTypedArray())
+			"v" -> addVertex(array.drop(1).toTypedArray())
+			"vt" -> addVertexTexture(array.drop(1).toTypedArray())
+			"vn" -> addVertexNormal(array.drop(1).toTypedArray())
 		}
 	}
 
 	fclose(file)
-}
-
-private fun addVertex(array: Array<String>) {
-	val vertex = if (array.size == 4) {
-		Vertex(array[0].toFloat(), array[1].toFloat(), array[2].toFloat(), array[3].toFloat())
-	} else {
-		Vertex(array[0].toFloat(), array[1].toFloat(), array[2].toFloat(), 1.0f)
-	}
-	vertices.add(vertex)
 }
 
 private fun addFace(array: Array<String>) {
@@ -41,4 +34,31 @@ private fun addFace(array: Array<String>) {
 	} else {
 		println("Error: Invalid vertex index in face definition - ${array.joinToString(" ")}")
 	}
+}
+
+private fun addVertex(array: Array<String>) {
+	val vertex = when (array.size) {
+		3 -> Vertex(array[0].toFloat(), array[1].toFloat(), array[2].toFloat())
+		4 -> Vertex(array[0].toFloat(), array[1].toFloat(), array[2].toFloat(), array[3].toFloat())
+		else -> throw Exception("Vertex error!")
+	}
+	vertices.add(vertex)
+}
+
+private fun addVertexTexture(array: Array<String>) {
+	val vertex = when (array.size) {
+		1 -> Vertex(array[0].toFloat(), 0.0f, 0.0f)
+		2 -> Vertex(array[0].toFloat(), array[1].toFloat(), 0.0f)
+		3 -> Vertex(array[0].toFloat(), array[1].toFloat(), array[2].toFloat())
+		else -> throw Exception("Vertex texture error!")
+	}
+	verticesTexture.add(vertex)
+}
+
+private fun addVertexNormal(array: Array<String>) {
+	val vertex = when (array.size) {
+		3 -> Vertex(array[0].toFloat(), array[1].toFloat(), array[2].toFloat())
+		else -> throw Exception("Vertex normal error!")
+	}
+	verticesNormal.add(vertex)
 }
