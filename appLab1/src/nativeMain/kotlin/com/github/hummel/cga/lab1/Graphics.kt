12a7e5e298ca65c19767b102b828ce29781e31e8
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-03-06 22:25:44 +0300

    mace

--- appLab1/src/nativeMain/kotlin/com/github/hummel/cga/lab1/Graphics.kt
+++ appLab1/src/nativeMain/kotlin/com/github/hummel/cga/lab1/Graphics.kt
@@ -4,7 +4,7 @@ import kotlinx.cinterop.*
 import platform.windows.*
 import kotlin.math.abs
 
-private val chunks: Int = faces.size / 1000
+private val chunks: Int = faces.size / 1000 + 2
 private val splitFaces: Array<List<Face>> = split(faces, chunks)
 private val white: Color = Color(-1, -1, -1)
 
