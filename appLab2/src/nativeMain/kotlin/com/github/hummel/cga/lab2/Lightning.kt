7b946b0564086a72e610352a3c5261322873959e
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-03-12 14:26:07 +0300

    better

--- appLab2/src/nativeMain/kotlin/com/github/hummel/cga/lab2/Lightning.kt
+++ appLab2/src/nativeMain/kotlin/com/github/hummel/cga/lab2/Lightning.kt
@@ -1,10 +1,12 @@
 package com.github.hummel.cga.lab2
 
-inline fun getFromLighting(face: Face): Color {
+const val generalIntencity: Float = 1.0f
+
+inline fun getColor(face: Face): Color {
 	val point = face.vertices[0]
 	val normal = face.poliNormal
 
-	val light = calculateLightDiffuse(point, normal)
+	val light = calculateLight(point, normal)
 
 	val colorVal = (if (light * 255 > 255) 255 else light * 255).toByte()
 
@@ -13,13 +15,13 @@ inline fun getFromLighting(face: Face): Color {
 	return color
 }
 
-inline fun calculateLightDiffuse(point: Vertex, normal: Vertex): Float {
+inline fun calculateLight(point: Vertex, normal: Vertex): Float {
 	val ray = lightPos - point
 	var lightResult = 0.0f
 	val angle = normal scalarMul ray
 
 	if (angle > 0) {
-		lightResult = 1.0f * angle / (ray.magnitude * normal.magnitude)
+		lightResult = generalIntencity * angle / (ray.magnitude * normal.magnitude)
 	}
 
 	return lightResult
--- appLab3/src/nativeMain/kotlin/com/github/hummel/cga/lab3/Lightning.kt
+++ appLab3/src/nativeMain/kotlin/com/github/hummel/cga/lab3/Lightning.kt
@@ -1,26 +1,49 @@
 package com.github.hummel.cga.lab3
 
-inline fun getFromLighting(face: Face, alpha: Float, beta: Float, gamma: Float): Color {
-	val point = face.vertices[0]
-	val normal = face.poliNormal
+import kotlin.math.pow
 
-	val light = calculateLightDiffuse(point, normal)
+const val generalIntencity: Float = 1.0f
 
-	val colorVal = (if (light * 255 > 255) 255 else light * 255).toByte()
+const val diffuseIntencity: Float = 0.2f
+const val specularIntencity: Float = 0.8f
 
-	val color = Color(colorVal, colorVal, colorVal)
+inline fun getColor(face: Face, alpha: Float, beta: Float, gamma: Float): Color {
+	val point = face.getCenteredVecForVertices(alpha, beta, gamma)
+	val normal = face.getCenteredVecForNormals(alpha, beta, gamma).normalize()
+
+	val light = calculateLight(point, normal)
+
+	val colorValR = (if (light.x * 255 > 255) 255 else light.x * 255).toByte()
+	val colorValG = (if (light.y * 255 > 255) 255 else light.y * 255).toByte()
+	val colorValB = (if (light.z * 255 > 255) 255 else light.z * 255).toByte()
+
+	val color = Color(colorValR, colorValG, colorValB)
 
 	return color
 }
 
-inline fun calculateLightDiffuse(point: Vertex, normal: Vertex): Float {
+inline fun calculateLight(point: Vertex, normal: Vertex): Vertex {
 	val ray = lightPos - point
-	var lightResult = 0.0f
+	var lightResult = Vertex(0.0f, 0.0f, 0.0f)
+
 	val angle = normal scalarMul ray
 
 	if (angle > 0) {
-		lightResult = 1.0f * angle / (ray.magnitude * normal.magnitude)
+		lightResult += generalIntencity * diffuseIntencity * angle / (ray.magnitude * normal.magnitude)
+	}
+
+	val refr = normal * 2.0f * angle - ray
+	val view = eye - point
+	val rDotV = refr scalarMul view
+	if (rDotV > 0) {
+		lightResult += generalIntencity * specularIntencity * (rDotV / (refr.magnitude * view.magnitude)).pow(2.0f)
 	}
 
 	return lightResult
 }
+
+inline fun Face.getCenteredVecForNormals(alpha: Float, beta: Float, gamma: Float): Vertex =
+	(normals[0] * alpha + normals[1] * beta + normals[2] * gamma)
+
+inline fun Face.getCenteredVecForVertices(alpha: Float, beta: Float, gamma: Float): Vertex =
+	vertices[0] * alpha + vertices[1] * beta + vertices[2] * gamma
