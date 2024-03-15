d7d209fa19b4dcf656c8f1ae13a0de901df29515
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-03-15 14:56:17 +0300

    Пока просто копия

---
+++ appLab4/src/nativeMain/kotlin/com/github/hummel/cga/lab3/Graphics.kt
@@ -0,0 +1,137 @@
+package com.github.hummel.cga.lab4
+
+import kotlinx.cinterop.*
+import platform.windows.*
+
+private val zBuffer: FloatArray = FloatArray(width * height)
+
+lateinit var displayMatrix: Array<FloatArray>
+lateinit var lightPos: Vertex
+lateinit var eyePos: Vertex
+
+fun renderObject(eye: Vertex) {
+	displayMatrix = getDisplayMatrix(eye)
+	lightPos = getLightPos(eye)
+	eyePos = eye
+
+	bitmapData.fill(0)
+	zBuffer.fill(Float.POSITIVE_INFINITY)
+
+	memScoped {
+		val params = Array(kernels) {
+			alloc<IntVar>()
+		}
+
+		params.forEachIndexed { index, param -> param.value = index }
+
+		val threads = Array(kernels) {
+			CreateThread(null, 0u, staticCFunction(::tfDrawVertices), params[it].ptr, 0u, null)
+		}
+
+		for (thread in threads) {
+			WaitForSingleObject(thread, INFINITE)
+			CloseHandle(thread)
+		}
+	}
+}
+
+private fun tfDrawVertices(lpParameter: LPVOID?): DWORD {
+	val parameter = lpParameter?.reinterpret<IntVar>()?.pointed?.value!!
+
+	for (face in threadFaces[parameter]) {
+		drawTriangle(face)
+	}
+
+	return 0u
+}
+
+private inline fun drawTriangle(face: Face) {
+	val viewDir = -face.vertices[0] + eyePos
+	val cosAngle = (face.poliNormal / face.normals.size.toFloat()) scalarMul viewDir
+
+	if (cosAngle <= 0) {
+		return
+	}
+
+	val drawFace = Face(
+		arrayOf(
+			multiplyVertexByMatrix(face.vertices[0], displayMatrix),
+			multiplyVertexByMatrix(face.vertices[1], displayMatrix),
+			multiplyVertexByMatrix(face.vertices[2], displayMatrix)
+		), face.normals, face.poliNormal
+	)
+
+	var minY = Int.MAX_VALUE
+	var maxY = Int.MIN_VALUE
+
+	for (vertex in drawFace.vertices) {
+		val y = vertex.y.toInt()
+		if (y < minY) {
+			minY = y
+		}
+		if (y > maxY) {
+			maxY = y
+		}
+	}
+
+	// Создать цикл по каждой строке изображения
+	for (y in minY..maxY) {
+		if (y in 0 until height) {
+			// Найти пересечения текущей строки с каждой из сторон треугольника
+			val xIntersections = IntArray(2)
+			var intersectionCount = 0
+			for (i in 0..2) {
+				val v0 = drawFace.vertices[i]
+				val v1 = drawFace.vertices[(i + 1) % 3]
+				val y0 = v0.y.toInt()
+				val y1 = v1.y.toInt()
+				if (y in y0 until y1 || y in y1 until y0) {
+					val t = (y - y0) / (y1 - y0).toFloat()
+					val x = (v0.x + t * (v1.x - v0.x)).toInt()
+					xIntersections[intersectionCount] = x
+					intersectionCount++
+				}
+			}
+
+			// Отсортировать пересечения по возрастанию
+			if (intersectionCount == 2 && xIntersections[0] > xIntersections[1]) {
+				val temp = xIntersections[0]
+				xIntersections[0] = xIntersections[1]
+				xIntersections[1] = temp
+			}
+
+			// Заполнить пиксели между пересечениями цветом треугольника
+			if (intersectionCount == 2) {
+				for (x in xIntersections[0]..xIntersections[1]) {
+					if (x in 0 until width) {
+						val v0 = drawFace.vertices[0]
+						val v1 = drawFace.vertices[1]
+						val v2 = drawFace.vertices[2]
+						val alpha =
+							((v1.y - v2.y) * (x - v2.x) + (v2.x - v1.x) * (y - v2.y)) / ((v1.y - v2.y) * (v0.x - v2.x) + (v2.x - v1.x) * (v0.y - v2.y))
+						val beta =
+							((v2.y - v0.y) * (x - v2.x) + (v0.x - v2.x) * (y - v2.y)) / ((v1.y - v2.y) * (v0.x - v2.x) + (v2.x - v1.x) * (v0.y - v2.y))
+						val gamma = 1 - alpha - beta
+						val zFragment = alpha * v0.z + beta * v1.z + gamma * v2.z
+
+						if (zBuffer[x * height + y] > zFragment) {
+							zBuffer[x * height + y] = zFragment
+
+							val color = getColor(face, alpha, beta, gamma)
+
+							setPixel(x, y, color)
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+private inline fun setPixel(x: Int, y: Int, color: Color) {
+	val offset = (y * width + x) shl 2
+	bitmapData[offset + 0] = color.blue
+	bitmapData[offset + 1] = color.green
+	bitmapData[offset + 2] = color.red
+	bitmapData[offset + 3] = -1
+}
---
+++ appLab4/src/nativeMain/kotlin/com/github/hummel/cga/lab3/Lightning.kt
@@ -0,0 +1,49 @@
+package com.github.hummel.cga.lab4
+
+import kotlin.math.pow
+
+const val generalIntencity: Float = 0.8f
+
+const val diffuseIntencity: Float = 1.0f
+const val specularIntencity: Float = 1.0f
+
+inline fun getColor(face: Face, alpha: Float, beta: Float, gamma: Float): Color {
+	val point = face.getCenteredVecForVertices(alpha, beta, gamma)
+	val normal = face.getCenteredVecForNormals(alpha, beta, gamma).normalize()
+
+	val light = calculateLight(point, normal)
+
+	val colorVal = (if (light * 255 > 255) 255 else light * 255).toByte()
+
+	val color = Color(colorVal, colorVal, colorVal)
+
+	return color
+}
+
+inline fun calculateLight(point: Vertex, normal: Vertex): Float {
+	//diffuse
+	val ray = lightPos - point
+	var lightResult = 0.0f
+	val angle = normal scalarMul ray
+
+	if (angle > 0) {
+		lightResult += generalIntencity * diffuseIntencity * angle / (ray.magnitude * normal.magnitude)
+	}
+
+	//specular
+	val refr = normal * 2.0f * angle - ray
+	val view = eyePos - point
+	val rDotV = refr scalarMul view
+
+	if (rDotV > 0) {
+		lightResult += generalIntencity * specularIntencity * (rDotV / (refr.magnitude * view.magnitude)).pow(2.0f)
+	}
+
+	return lightResult
+}
+
+inline fun Face.getCenteredVecForNormals(alpha: Float, beta: Float, gamma: Float): Vertex =
+	normals[0] * alpha + normals[1] * beta + normals[2] * gamma
+
+inline fun Face.getCenteredVecForVertices(alpha: Float, beta: Float, gamma: Float): Vertex =
+	vertices[0] * alpha + vertices[1] * beta + vertices[2] * gamma
