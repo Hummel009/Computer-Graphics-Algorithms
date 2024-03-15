28a47b4768875d230c1632a5e1dfb79382274c21
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-03-15 13:34:32 +0300

    retrofix for 2

--- appLab2/src/nativeMain/kotlin/com/github/hummel/cga/lab2/Main.kt
+++ appLab2/src/nativeMain/kotlin/com/github/hummel/cga/lab2/Main.kt
@@ -2,12 +2,25 @@ package com.github.hummel.cga.lab2
 
 import kotlinx.cinterop.*
 import platform.windows.*
+import kotlin.math.cos
 import kotlin.math.max
+import kotlin.math.sin
 import kotlin.time.measureTime
 
 private const val VK_Z: Int = 0x5A
 private const val VK_X: Int = 0x58
 private const val VK_C: Int = 0x43
+
+private var rotateX: Float = 0.0f
+private var rotateY: Float = 0.0f
+private var prevMouseX: Int = 0
+private var prevMouseY: Int = 0
+private var isDragging: Boolean = false
+
+private val execTimes: MutableList<Long> = mutableListOf()
+private val execTime: Array<Long> = arrayOf(0, 0)
+private var min: Long = Long.MAX_VALUE
+private var max: Long = Long.MIN_VALUE
 
 const val width: Int = 1040
 const val height: Int = 580
@@ -15,11 +28,19 @@ const val height: Int = 580
 var faces: MutableList<Face> = ArrayList()
 var bitmapData: ByteArray = ByteArray(width * height * 4)
 
-lateinit var name: String
+private var dist: Float = 0.0f
 
 fun main() {
 	print("Enter model name (tie|mace|knight): ")
-	name = readln()
+
+	val name = readln()
+
+	dist = when (name) {
+		"tie" -> 10.0f
+		"knight" -> 20.0f
+		"mace" -> 100.0f
+		else -> 50.0f
+	}
 
 	parse("$name.obj")
 
@@ -70,27 +91,50 @@ fun main() {
 	}
 }
 
-private val execTimes: MutableList<Long> = mutableListOf()
-private val execTime: Array<Long> = arrayOf(0, 0)
-private var min: Long = Long.MAX_VALUE
-private var max: Long = Long.MIN_VALUE
 private fun wndProc(window: HWND?, msg: UINT, wParam: WPARAM, lParam: LPARAM): LRESULT {
 	when (msg.toInt()) {
+		WM_MOUSEMOVE -> {
+			if (isDragging) {
+				val currMouseX = lParam.toInt() and 0xFFFF
+				val currMouseY = (lParam.toInt() shr 16) and 0xFFFF
+
+				rotateY += (currMouseX - prevMouseX) / 200.0f
+				rotateX += (currMouseY - prevMouseY) / 200.0f
+
+				prevMouseX = currMouseX
+				prevMouseY = currMouseY
+			}
+
+			InvalidateRect(window, null, FALSE)
+		}
+
+		WM_LBUTTONDOWN -> {
+			isDragging = true
+			prevMouseX = lParam.toInt() and 0xFFFF
+			prevMouseY = (lParam.toInt() shr 16) and 0xFFFF
+			SetCapture(window)
+		}
+
+		WM_LBUTTONUP -> {
+			isDragging = false
+			ReleaseCapture()
+		}
+
 		WM_KEYDOWN -> {
 			val time = measureTime {
 				when (wParam.toInt()) {
 					VK_Z -> {
-						rotateVerticesAxisZ()
+						rotateVertices("z")
 						InvalidateRect(window, null, FALSE)
 					}
 
 					VK_X -> {
-						rotateVerticesAxisX()
+						rotateVertices("x")
 						InvalidateRect(window, null, FALSE)
 					}
 
 					VK_C -> {
-						rotateVerticesAxisY()
+						rotateVertices("y")
 						InvalidateRect(window, null, FALSE)
 					}
 
@@ -108,7 +152,13 @@ private fun wndProc(window: HWND?, msg: UINT, wParam: WPARAM, lParam: LPARAM): L
 					val hdc = BeginPaint(window, ps.ptr)
 					val hdcMem = CreateCompatibleDC(hdc)
 
-					renderObject()
+					val eye = Vertex(
+						dist * cos(rotateX) * cos(rotateY),
+						dist * sin(rotateX),
+						dist * cos(rotateX) * sin(rotateY)
+					)
+
+					renderObject(eye)
 
 					val hBitmap = CreateBitmap(width, height, 1u, 32u, bitmapData.refTo(0))
 					val hOldBitmap = SelectObject(hdcMem, hBitmap)
@@ -135,6 +185,8 @@ private fun wndProc(window: HWND?, msg: UINT, wParam: WPARAM, lParam: LPARAM): L
 			val avg = execTimes.average().toLong()
 
 			println("$fps FPS, [$min; $max]; AVG: $avg; MLag: ${execTime[0] / 1_000_000}ms; GLag: ${execTime[1] / 1_000_000}ms")
+
+			execTime.fill(0)
 		}
 
 		WM_CLOSE -> DestroyWindow(window)
