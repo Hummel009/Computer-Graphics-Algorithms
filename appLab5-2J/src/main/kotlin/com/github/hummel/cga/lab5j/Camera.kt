a22ebc12c34f84ef0cc9fd2b070ad94eb65584ef
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-04-09 17:42:55 +0300

    2

---
+++ appLab5-2J/src/main/kotlin/com/github/hummel/cga/lab5j/Camera.kt
@@ -0,0 +1,46 @@
+package com.github.hummel.cga.lab5j
+
+import kotlin.math.PI
+import kotlin.math.tan
+
+private val target: Vertex = Vertex(0.0f, 0.0f, 0.0f)
+private val up: Vertex = Vertex(0.0f, 1.0f, 0.0f)
+
+private var fov: Float = PI.toFloat() / 4.0f
+private var aspect = windowWidth.toFloat() / windowHeight.toFloat()
+
+private var zNear: Float = 1.0f
+private var zFar: Float = 100.0f
+
+private val matrixProjection: Array<FloatArray> = arrayOf(
+	floatArrayOf(1.0f / (aspect * tan(fov / 2.0f)), 0.0f, 0.0f, 0.0f),
+	floatArrayOf(0.0f, 1.0f / (tan(fov / 2.0f)), 0.0f, 0.0f),
+	floatArrayOf(0.0f, 0.0f, zFar / (zNear - zFar), (zNear * zFar) / (zNear - zFar)),
+	floatArrayOf(0.0f, 0.0f, -1.0f, 0.0f)
+)
+
+private val matrixViewport: Array<FloatArray> = arrayOf(
+	floatArrayOf(windowWidth.toFloat() / 2.0f, 0.0f, 0.0f, windowWidth.toFloat() / 2.0f),
+	floatArrayOf(0.0f, windowHeight.toFloat() / 2.0f, 0.0f, windowHeight.toFloat() / 2.0f),
+	floatArrayOf(0.0f, 0.0f, 1.0f, 0.0f),
+	floatArrayOf(0.0f, 0.0f, 0.0f, 1.0f)
+)
+
+private val staticMultiplier: Array<FloatArray> = multiplyMatrixByMatrix(matrixViewport, matrixProjection)
+
+fun getDisplayMatrix(eye: Vertex): Array<FloatArray> {
+	val zAxis = (eye - target).normalize()
+	val xAxis = (up vectorMul zAxis).normalize()
+	val yAxis = xAxis vectorMul zAxis
+
+	val matrixView = arrayOf(
+		floatArrayOf(xAxis.x, xAxis.y, xAxis.z, -(xAxis scalarMul eye)),
+		floatArrayOf(yAxis.x, yAxis.y, yAxis.z, -(yAxis scalarMul eye)),
+		floatArrayOf(zAxis.x, zAxis.y, zAxis.z, -(zAxis scalarMul eye)),
+		floatArrayOf(0.0f, 0.0f, 0.0f, 1.0f)
+	)
+
+	return multiplyMatrixByMatrix(staticMultiplier, matrixView)
+}
+
+fun getLightPos(eye: Vertex): Vertex = eye + up
---
+++ appLab5-2J/src/main/kotlin/com/github/hummel/cga/lab5j/Math.kt
@@ -0,0 +1,27 @@
+package com.github.hummel.cga.lab5j
+
+fun multiplyVertexByMatrix(vertex: Vertex, matrix: Array<FloatArray>): Vertex {
+	val result = FloatArray(4)
+	val vectorData = floatArrayOf(vertex.x, vertex.y, vertex.z, vertex.w)
+
+	for (i in 0 until 4) {
+		for (j in 0 until 4) {
+			result[i] += matrix[i][j] * vectorData[j]
+		}
+	}
+
+	return Vertex(result[0], result[1], result[2], result[3])
+}
+
+fun multiplyMatrixByMatrix(matrixA: Array<FloatArray>, matrixB: Array<FloatArray>): Array<FloatArray> {
+	val result = Array(4) { FloatArray(4) }
+
+	for (i in 0 until 4) {
+		for (j in 0 until 4) {
+			val sum = (0 until 4).asSequence().map { matrixA[i][it] * matrixB[it][j] }.sum()
+			result[i][j] = sum
+		}
+	}
+
+	return result
+}
