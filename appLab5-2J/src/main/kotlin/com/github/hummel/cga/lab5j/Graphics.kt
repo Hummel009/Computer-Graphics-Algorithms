6844fc23dfdeaa2a92d28b6aa453d4a0a99e37a9
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-04-09 18:46:52 +0300

    Update Graphics.kt

--- appLab5-2J/src/main/kotlin/com/github/hummel/cga/lab5j/Graphics.kt
+++ appLab5-2J/src/main/kotlin/com/github/hummel/cga/lab5j/Graphics.kt
@@ -1,6 +1,11 @@
 package com.github.hummel.cga.lab5j
 
+import org.lwjgl.BufferUtils
+import org.lwjgl.opengl.GL
 import org.lwjgl.opengl.GL11.*
+import org.lwjgl.opengl.GL15.*
+import org.lwjgl.opengl.GL20.*
+import org.lwjgl.opengl.GL30.*
 
 lateinit var displayMatrix: Array<FloatArray>
 lateinit var lightPos: Vertex
@@ -15,25 +20,27 @@ fun renderObject(eye: Vertex) {
 }
 
 private fun drawTriangle(face: Face) {
-	val viewDir = -face.realVertices[0] + eyePos
-	val cosAngle = face.poliNormal scalarMul viewDir
-
-	if (cosAngle <= 0) {
-		return
-	}
-
 	face.viewVertices[0] = multiplyVertexByMatrix(face.realVertices[0], displayMatrix)
 	face.viewVertices[1] = multiplyVertexByMatrix(face.realVertices[1], displayMatrix)
 	face.viewVertices[2] = multiplyVertexByMatrix(face.realVertices[2], displayMatrix)
 
-	val rgb = getResultRgb(face).toGL()
+	for (i in face.viewVertices.indices) {
+		face.viewVertices[i].z *= -1
+	}
 
 	for (i in face.viewVertices.indices) {
-		face.savedW[i] = face.viewVertices[i].w
 		face.viewVertices[i] divSelf face.viewVertices[i].w
 	}
 
+	val rgb = getResultRgb(face).toGL()
+
 	glColor3f(rgb.r, rgb.g, rgb.b)
+
+	glEnable(GL_DEPTH_TEST)
+	glDepthFunc(GL_LEQUAL)
+
+	glEnable(GL_CULL_FACE)
+	glCullFace(GL_BACK)
 
 	glBegin(GL_TRIANGLES)
 	for ((x, y, z, _) in face.viewVertices) {
