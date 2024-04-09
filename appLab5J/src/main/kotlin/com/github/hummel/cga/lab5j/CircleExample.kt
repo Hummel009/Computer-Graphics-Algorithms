3e681a4ff20bf0c4b14801120e620e1a20ace87f
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-04-09 14:37:13 +0300

    ok

--- appLab5J/src/main/kotlin/com/github/hummel/cga/lab5j/CircleExample.kt
+++ appLab5J/src/main/kotlin/com/github/hummel/cga/lab5j/CircleExample.kt
@@ -1,10 +1,10 @@
 package com.github.hummel.cga.lab5j
 
 import org.lwjgl.glfw.Callbacks
-import org.lwjgl.glfw.GLFW
+import org.lwjgl.glfw.GLFW.*
 import org.lwjgl.glfw.GLFWErrorCallback
 import org.lwjgl.opengl.GL
-import org.lwjgl.opengl.GL11
+import org.lwjgl.opengl.GL11.*
 import org.lwjgl.system.MemoryUtil
 
 fun main() {
@@ -19,65 +19,72 @@ object OpenGL {
 		loop()
 
 		Callbacks.glfwFreeCallbacks(window)
-		GLFW.glfwDestroyWindow(window)
+		glfwDestroyWindow(window)
 
-		GLFW.glfwTerminate()
+		glfwTerminate()
 	}
 
 	private fun init() {
-		GLFW.glfwSetErrorCallback(GLFWErrorCallback.createPrint(System.err))
+		glfwSetErrorCallback(GLFWErrorCallback.createPrint(System.err))
 
-		check(GLFW.glfwInit()) { "Unable to initialize GLFW" }
+		check(glfwInit()) { "Unable to initialize GLFW" }
 
-		GLFW.glfwDefaultWindowHints()
-		GLFW.glfwWindowHint(GLFW.GLFW_VISIBLE, GLFW.GLFW_FALSE)
-		GLFW.glfwWindowHint(GLFW.GLFW_RESIZABLE, GLFW.GLFW_FALSE)
+		glfwDefaultWindowHints()
+		glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE)
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE)
 
-		val videoMode = GLFW.glfwGetVideoMode(GLFW.glfwGetPrimaryMonitor()) ?: return
+		val videoMode = glfwGetVideoMode(glfwGetPrimaryMonitor()) ?: return
 		val screenWidth = videoMode.width()
 		val screenHeight = videoMode.height()
 
 		val xPos = (screenWidth - windowWidth) / 2
 		val yPos = (screenHeight - windowHeight) / 2
 
-		window = GLFW.glfwCreateWindow(windowWidth, windowHeight, "Black Window", MemoryUtil.NULL, MemoryUtil.NULL)
+		window = glfwCreateWindow(windowWidth, windowHeight, "Black Window", MemoryUtil.NULL, MemoryUtil.NULL)
 		if (window == MemoryUtil.NULL) {
 			throw RuntimeException("Failed to create the GLFW window")
 		}
 
-		GLFW.glfwSetWindowPos(window, xPos, yPos)
+		glfwSetWindowPos(window, xPos, yPos)
 
-		GLFW.glfwSetKeyCallback(window) { window: Long, key: Int, _: Int, action: Int, _: Int ->
-			if (key == GLFW.GLFW_KEY_ESCAPE && action == GLFW.GLFW_RELEASE) {
-				GLFW.glfwSetWindowShouldClose(window, true)
+		glfwSetKeyCallback(window) { window: Long, key: Int, _: Int, action: Int, _: Int ->
+			if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE) {
+				glfwSetWindowShouldClose(window, true)
 			}
 		}
 
-		GLFW.glfwMakeContextCurrent(window)
-		GLFW.glfwSwapInterval(1)
-		GLFW.glfwShowWindow(window)
+		glfwMakeContextCurrent(window)
+		glfwSwapInterval(1)
+		glfwShowWindow(window)
 
 		GL.createCapabilities()
 	}
 
 	private fun loop() {
-		while (!GLFW.glfwWindowShouldClose(window)) {
+		while (!glfwWindowShouldClose(window)) {
 			drawSquare() // Рисуем квадрат
-			GLFW.glfwSwapBuffers(window)
-			GLFW.glfwPollEvents()
+			glfwSwapBuffers(window)
+			glfwPollEvents()
 		}
 	}
 
 	private fun drawSquare() {
-		GL11.glClearColor(0.0f, 0.0f, 0.0f, 1.0f)
-		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT or GL11.GL_DEPTH_BUFFER_BIT)
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f)
+		glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT)
 
-		GL11.glBegin(GL11.GL_QUADS)
-		GL11.glColor3f(1.0f, 1.0f, 1.0f)
-		GL11.glVertex2f(-0.5f, -0.5f)
-		GL11.glVertex2f(0.5f, -0.5f)
-		GL11.glVertex2f(0.5f, 0.5f)
-		GL11.glVertex2f(-0.5f, 0.5f)
-		GL11.glEnd()
+		glMatrixMode(GL_PROJECTION)
+		glLoadIdentity()
+		glOrtho(0.0, windowWidth.toDouble(), windowHeight.toDouble(), 0.0, -1.0, 1.0)
+
+		glMatrixMode(GL_MODELVIEW)
+		glLoadIdentity()
+
+		glBegin(GL_QUADS)
+		glColor3f(1.0f, 1.0f, 1.0f)
+		glVertex2f(100.0f, 100.0f)
+		glVertex2f(200.0f, 100.0f)
+		glVertex2f(200.0f, 200.0f)
+		glVertex2f(100.0f, 200.0f)
+		glEnd()
 	}
 }
