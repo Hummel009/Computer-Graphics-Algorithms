348ce33f40bdb2a1566d204bc3f99294d4a57d1c
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-04-10 17:43:32 +0300

    Revert "invert Z axis"
    This reverts commit 0b648652bd6beb7afec5f9e1a1612218348f3faf.

--- appLab2J/src/main/kotlin/com/github/hummel/cga/lab2j/Graphics.kt
+++ appLab2J/src/main/kotlin/com/github/hummel/cga/lab2j/Graphics.kt
@@ -29,10 +29,6 @@ private fun drawTriangle(face: Face) {
 	face.viewVertices[2] = multiplyVertexByMatrix(face.realVertices[2], displayMatrix)
 
 	for (i in face.viewVertices.indices) {
-		face.viewVertices[i].z *= -1
-	}
-
-	for (i in face.viewVertices.indices) {
 		face.savedW[i] = face.viewVertices[i].w
 		face.viewVertices[i] divSelf face.viewVertices[i].w
 	}
