866d0c18671317f1e9f0d169f7b091469978a935
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-04-10 18:17:46 +0300

    Kotlin 1.9.23, Gradle 8.7

--- appLab5-4J/build.gradle.kts
+++ appLab5-4J/build.gradle.kts
@@ -2,7 +2,7 @@ import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 plugins {
-	id("org.jetbrains.kotlin.jvm") version "1.9.22"
+	id("org.jetbrains.kotlin.jvm") version "1.9.23"
 	id("application")
 }
 
@@ -14,7 +14,7 @@ val embed: Configuration by configurations.creating
 dependencies {
 	embed(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
 
-	embed("org.jetbrains.kotlin:kotlin-stdlib:1.9.22")
+	embed("org.jetbrains.kotlin:kotlin-stdlib:1.9.23")
 
 	embed("com.formdev:flatlaf:3.2.5")
 	embed("com.formdev:flatlaf-intellij-themes:3.2.5")
