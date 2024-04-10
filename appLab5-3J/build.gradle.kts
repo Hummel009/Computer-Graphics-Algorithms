3185866ba28290fbfb454a5ecf12ca5635a45d4b
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-04-10 20:33:03 +0300

    lib update

--- appLab5-3J/build.gradle.kts
+++ appLab5-3J/build.gradle.kts
@@ -16,13 +16,13 @@ dependencies {
 
 	embed("org.jetbrains.kotlin:kotlin-stdlib:1.9.23")
 
-	embed("com.formdev:flatlaf:3.2.5")
-	embed("com.formdev:flatlaf-intellij-themes:3.2.5")
+	embed("com.formdev:flatlaf:3.4.1")
+	embed("com.formdev:flatlaf-intellij-themes:3.4.1")
 
 	implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
 
-	implementation("com.formdev:flatlaf:3.2.5")
-	implementation("com.formdev:flatlaf-intellij-themes:3.2.5")
+	implementation("com.formdev:flatlaf:3.4.1")
+	implementation("com.formdev:flatlaf-intellij-themes:3.4.1")
 }
 
 java {
